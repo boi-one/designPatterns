030c011a8307c685abae18b00b753be3a856eae8
boi-one <boi.one>
2023-12-19 14:04:07 +0100

    changes

--- Assets/Scripts/ObjectPool/Inputs.cs
+++ Assets/Scripts/ObjectPool/Inputs.cs
@@ -12,10 +12,13 @@ public class Inputs : MonoBehaviour
     [SerializeField] private float cooldown;
     private float tempTime;
     [SerializeField] private float bulletSpeed;
+    private Rigidbody2D rb;
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        
         bulletPool = new ObjectPool<Bullet>(bulletPrefab.GetComponent<Bullet>());
-        for (int i = 0; i < 30; i++)
+        for (int i = 0; i < 12; i++)
         {
             Bullet bullet = bulletPool.NewObject();
             bullet.Setup(bulletPool);
@@ -26,7 +29,9 @@ public class Inputs : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         Vector3 screenBorder = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height,0));
+        Debug.Log(-screenBorder.x + " " + -screenBorder.y);
         if (Input.GetKey(KeyCode.Mouse0) && Time.time > tempTime)
         {
             tempTime = Time.time + cooldown;
@@ -37,8 +42,8 @@ public class Inputs : MonoBehaviour
 
         foreach (Bullet b in bulletPool.activePool.ToList())
         {
-            if (b.transform.position.x > screenBorder.x || b.transform.position.x < -screenBorder.x ||
-                b.transform.position.y > screenBorder.y || b.transform.position.y < -screenBorder.y )
+            if (b.transform.position.x > screenBorder.x || b.transform.position.x < transform.position.x - 10 /*-screenBorder.x*/ ||
+                b.transform.position.y > screenBorder.y || b.transform.position.y < transform.position.y - 10 /*-screenBorder.y*/ )
                 bulletPool.Inactive(b);
         }
     }
